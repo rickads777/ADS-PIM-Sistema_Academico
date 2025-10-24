fdd2a02641322553866f270e172a972da0acc11f
lucas3984 <lucasg3984@gmail.com>
2025-10-24 17:53:47 -0300

    elementos básicos

--- Python_Flet/main.py
+++ Python_Flet/main.py
@@ -4,5 +4,29 @@ nomeApp = "EducaZone"
 
 def main(page: ft.Page):
     page.title = nomeApp
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    field_Usuario = ft.TextField(hint_text="Usuário")
+    field_Senha = ft.TextField(hint_text="Senha")
+
+    campo_Principal = ft.Column( #Coluna (Campo) onde ficará disposto os elementos do login
+        controls=[
+            ft.Row(
+                controls=[
+                    field_Usuario
+                ]
+            ),
+            ft.Row(
+                controls=[
+                    field_Senha
+                ]
+            )
+        ]
+    )
     
+    #Capturar altura e largura do app
+    WIDTH: int = page.width
+    HEIGHT: int = page.height
+    
+    #adcionar elementos na página
+    page.add(campo_Principal)
 ft.app(target=main)
